6c6a0f6f0bd72a4c8b2f4348bc06fe665166c855
ankit saini <ankitsaini100205@gmail.com>
2020-05-11 12:26:31 +0530

    connecting databae

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 #[macro_use]
 extern crate clap;
-use std::io::{stdin, stdout, Write};
 use clap::App;
-
-fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-    if let Some(matches) = matches.subcommand_matches("note"){
-
-    }
+use std::io::{stdin, stdout, Write};
+mod note;
+use sqlx::{SqliteConnection, Connect, Executor};
+use tokio;
+// fn main() {
+//     let yaml = load_yaml!("cli.yml");
+//     let matches = App::from_yaml(yaml).get_matches();
+//     if let Some(matches) = matches.subcommand_matches("note"){
+//         let msg = matches.value_of("note").unwrap();
+//         note::add_new(String::from(msg));
+//     }
+// }
+#[tokio::main]
+async fn main()-> Result<(), sqlx::Error> {
+    let path = "/home/akanksha/code/rust/pat/data/data.db";
+    println!("{:?}", &format!("sqlite://{}", path));
+    let mut conn = SqliteConnection::connect(&format!("sqlite://{}", path)).await?;
+    let query = conn.execute("select * from notes").await?;
+    println!("{:?}", query);
+    conn.execute("insert into notes values (\"Hello\", 3)").await?;
+    Ok(())
 }
--- src/note.rs
+++ src/note.rs
@@ -1,25 +1,29 @@
-use std::SystemTime;
-
+use std::time::SystemTime;
+use chrono::prelude::{Local, DateTime};
+#[derive(Debug)]
 struct Note{
     msg: String,
-    time: SystemTime,
+    time: DateTime<Local>,
 }
 
 impl Note{
-    fn new(&self, msg: String)->Note{
+    fn new(msg: String)->Note{
         Note{
             msg: msg,
-            time: SystemTime::now()
+            time: Local::now()
         }
     }
+
 }
 
 pub fn add_new(msg: String){
+    let note = Note::new(msg).time.timestamp();
+}
+
+pub fn get_all(){
+    unimplemented!()
 
 }
-pub fn get_all() -> [Note]{
-
-}
-pub fn get_between_times(from: SystemTime, to: SystemTime) -> [Note]{
-
+pub fn get_between_times(from: SystemTime, to: SystemTime){
+    unimplemented!()
 }
